0d4085c09056c888eedb88dc66bd0a706ea0fab5
Joshua Miller <jmiller@vera.com>
2018-06-29 16:00:51 -0700

    Birthday, my spin

--- 03_Birthday_Count_Down/program.py
+++ 03_Birthday_Count_Down/program.py
@@ -3,27 +3,43 @@
 import datetime
 
 
-def print_banner():
+def banner():
     print('-----------------------------------------')
     print('         The Birthday App                ')
     print('-----------------------------------------')
 
 
 def get_birthday():
-    birth_year = input('What year where you born [YYYY]? ')
-    birth_day = input('What day where you born [DD]? ')
-    birth_month = input('What month where you born [MM]? ')
+    birth_year = int(input('What year where you born [YYYY]? '))
+    birth_day = int(input('What day where you born [DD]? '))
+    birth_month = int(input('What month where you born [MM]? '))
 
-    return (birth_year, birth_day, birth_month)
+    birthday = datetime.date(birth_year, birth_month, birth_day)
 
-def get_day_until_birthday(b_year,b_day,b_month):
-    today = datetime.datetime().now()
-    print(today)
+    return (birthday)
+
+def get_day_until_birthday(user_birthday):
+    today = datetime.date.today()
+    target_date = datetime.date(today.year,user_birthday.month,user_birthday.day)
+    difference = today - target_date
+    return difference.days
+
+def birthday_message(days_left):
+    if days_left == 0:
+        print('Happy Birthday')
+    elif days_left < 0:
+        print('Your Birthday is in {} days!'.format(-days_left))
+    elif days_left > 0:
+        print('Your Birthday was {} days ago!'.format(days_left))
+    else:
+        print('Error: Danger Wil Robinson, danger how did you get here')
 
 
-
-print_banner()
-b_year,b_day,b_month = get_birthday()
-days_left = get_day_until_birthday(b_year,b_day,b_month)
+banner()
+user_birthday = get_birthday()
+#print(user_birthday)
+days_left = get_day_until_birthday(user_birthday)
+#print(days_left)
+birthday_message(days_left)
 
 
