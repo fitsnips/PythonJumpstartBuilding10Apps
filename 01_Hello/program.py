1d75265dc43d68358c82620a858eff81d18db086
Joshua Miller <jmiller@vera.com>
2018-06-29 00:14:49 -0700

    Hello world with input

---
+++ 01_Hello/program.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+print('------------------------------------------')
+print('          Hello World App                 ')
+print('------------------------------------------')
+
+username = input('Hello, what is your name? ')
+print('Hello ' + username + ' it is nice to meet you.' )
