155752a3e792de7fb22f1cb2bfbc4cfb101c1bb3
Joshua Miller <jmiller@vera.com>
2018-07-06 13:00:13 -0500

    Daily Journal - completed

--- 04_Journal_App_with_io/journal.py
+++ 04_Journal_App_with_io/journal.py
@@ -1,16 +1,42 @@
 #!/usr/bin/env python3
 
+import os
 
-def open_journal(jname):
-    print('Opening journal {}'.format(jname))
+def load(jname):
+    """
+    This method creates a journal
+    :param jname: The journal file name
+    :return jentries: Array of lines of the journal file
+    """
+    filename = get_full_pathname(jname)
+    jentries = []
+
+    if os.path.exists(filename):
+        print('Reading in journal .... {}'.format(filename))
+        print()
+        with open(filename) as fin:
+            for entry in fin.readlines():
+                jentries.append(entry.rstrip())
+    return jentries
+
+def save(jname, journal_data):
+    filename = get_full_pathname(jname)
+    print('.... saving your journal to {}'.format(filename))
+
+    if journal_data:
+        with open(filename, 'w') as fout:
+            for entry in journal_data:
+                fout.write(entry + '\n')
 
 
-def add_entry(journal_entries):
-    entry = input('What would you like to add? ')
-    journal_entries.append(entry)
-    print('Adding entry to  journal {}'.format(entry))
-    #list_entries(journal_entries)
+def get_full_pathname(jname):
+    filename = os.path.abspath(os.path.join('.', 'journals', jname + '.jrl'))
+    return filename
 
+
+def add_entry(jdata, entry):
+    jdata.append(entry)
+    return jdata
 
 def list_entries(journal_entries):
     print('Your journal entries: ')
@@ -18,5 +44,4 @@ def list_entries(journal_entries):
     for key,entry in enumerate(entries):
         print('* [{}] {}'.format(key + 1,entry))
 
-def close_journal():
-    print('Closing journal {}'.format(jname))
+
--- 04_Journal_App_with_io/program.py
+++ 04_Journal_App_with_io/program.py
@@ -8,37 +8,47 @@ def banner():
     print('                Daily Journal                 ')
     print('----------------------------------------------')
 
+
 def menu():
-    menu_selection = input('What would you like to do with your journal? [L]ist, [A]dd, E[x]it').upper().strip()
+    menu_selection = input('What would you like to do with your journal? [L]ist, [A]dd, E[x]it ').upper().strip()
     return menu_selection
 
 
+def new_entry(j_entries):
+    entry = input('What would you like to add? ')
+    journal_entries = journal.add_entry(j_entries, entry)
+    print('Added entry to journal {}'.format(entry))
+    return journal_entries
 
+
+def quit(name, entries):
+    print()
+    journal.save(name, entries)
+    print('Goodbye')
+    sys.exit()
 
 
 def main():
-    journal_name = 'default.journal'
-    journal_entries = []
-    menu_choice = True
+    journal_name = 'default'
+    menu_choice = 'EMPTY'
     banner()
 
-    # Read in our journal
-    journal.open_journal(journal_name)
+    journal_entries = journal.load(journal_name)
 
-    while menu_choice != 'x':
+
+    while menu_choice != 'X' and menu_choice:
         menu_choice = menu()
 
         if menu_choice == 'L':
-            journal_entries = journal.list_entries(journal_entries)
+            journal.list_entries(journal_entries)
         elif menu_choice == 'A':
-            journal.add_entry(journal_entries)
-        elif menu_choice == 'X':
-            print()
-            print('Goodbye')
-            sys.exit()
-        else:
+            journal_entries = new_entry(journal_entries)
+        elif menu_choice != 'X' and menu_choice:
             print('Error: the sky is rising AND your entry is not understood')
 
+    quit(journal_name, journal_entries)
 
 
-main()
+
+if __name__ == '__main__':
+    main()
