2e59eaa510eda8a5246531b333c79f43372ee2ea
Joshua Miller <jmiller@vera.com>
2018-07-06 17:28:25 -0500

    Cats App - start

---
+++ 06_LOLCAT_Factory/program.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+def main():
+    banner()
+    # get or create output folder
+    # download cat files
+    # display cats
+
+def banner():
+    print('-----------------------------------')
+    print('          Cat Factory App          ')
+    print('-----------------------------------')
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
