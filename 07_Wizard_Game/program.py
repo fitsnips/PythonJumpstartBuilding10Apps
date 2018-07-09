6c3756edb0ee75492093b65fc56a036d491fbd8f
Joshua Miller <jmiller@vera.com>
2018-07-09 15:17:38 -0500

    07 Wizard App basic actions

--- 07_Wizard_Game/program.py
+++ 07_Wizard_Game/program.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python3
 
 
+
 def main():
     banner()
+    game_loop()
 
 
 def banner():
     print('-------------------------------------')
     print('          Wizard Game App            ')
     print('-------------------------------------')
+
+
+def game_loop():
+    while True:
+        cmd = input('Do you [a]ttack, [r]un away , or [l]ook around: ').rstrip().lower()
+        if cmd == 'a':
+            print('attack')
+        elif cmd == 'r':
+            print('run')
+        elif cmd == 'l':
+            print('look around')
+        else:
+            print('OK exiting game ... goodbye!')
+            break
+
+
 
 
 
