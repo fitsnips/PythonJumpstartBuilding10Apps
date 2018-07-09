7c8e11f7611880df526c71db525c175cd3fdcc29
Joshua Miller <jmiller@vera.com>
2018-07-09 14:14:58 -0500

    Cats App - working Cats app completed. Opens window on OSX, Windows, and Linux

--- 06_LOLCAT_Factory/cats_service.py
+++ 06_LOLCAT_Factory/cats_service.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
+import os
+import shutil
 import requests
 
 
 def get_cat(target_folder, file_name):
     url = 'http://consuming-python-services-api.azurewebsites.net/cats/random'
     data = get_data_from_url(url)
+    save_image(target_folder, file_name, data)
+
 
 def get_data_from_url(url):
-    response = requests.get(url)
+    response = requests.get(url, stream=True)
     return response.raw
+
+
+def save_image(folder, name, data):
+    filename = os.path.join(folder, name + '.jpg')
+    with open(filename, 'wb') as fout:
+        shutil.copyfileobj(data, fout)
 
--- 06_LOLCAT_Factory/program.py
+++ 06_LOLCAT_Factory/program.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import os
+import platform
+import subprocess
 import cats_service
 
 def main():
@@ -10,6 +12,7 @@ def main():
     # download cat files
     download_cats(folder)
     # display cats
+    show_cats(folder)
 
 
 def banner():
@@ -31,11 +34,26 @@ def get_or_create_output_folder():
     return full_path
 
 def download_cats(target_folder):
+    print('Contacting server to download cats ... ')
     cat_count = 8
     for i in range(1,cat_count+1):
         name = 'lolcat_{}'.format(i)
+        print('Downloading cat {}'.format(name))
         cats_service.get_cat(target_folder, name)
+    print('Downloading completed.')
 
+def show_cats(folder):
+    # open the folder in OS file
+    OS = platform.system()
+    print('Displaying cats in OS window: ')
+    if OS == 'Darwin':
+        subprocess.call(['open', folder])
+    elif OS == 'Windows':
+        subprocess.call(['explorer', folder])
+    elif OS == 'Linux':
+        subprocess.call(['xdg-open', folder])
+    else:
+        print('Sorry we do not support automated window browsing on the {} operating system'.format(OS))
 
 if __name__ == '__main__':
     main()
