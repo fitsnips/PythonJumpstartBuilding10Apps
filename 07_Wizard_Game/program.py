13de9a4fe8b284802893cb9a75ca67190a53ebbe
Joshua Miller <jmiller@vera.com>
2018-07-09 15:06:01 -0500

    07 Wizard App start

---
+++ 07_Wizard_Game/program.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+def main():
+    banner()
+
+
+def banner():
+    print('-------------------------------------')
+    print('          Wizard Game App            ')
+    print('-------------------------------------')
+
+
+
+if __name__ == '__main__':
+    main()
