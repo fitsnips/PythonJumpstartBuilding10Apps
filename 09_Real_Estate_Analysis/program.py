e9069a774a9ae19cf06621121a5320359a3d8a5c
Joshua Miller <jmiller@vera.com>
2018-07-13 19:28:30 -0500

    09 Real Estate Analysis - Really should have tried to run this before commiting - runs now

--- 09_Real_Estate_Analysis/program.py
+++ 09_Real_Estate_Analysis/program.py
@@ -4,9 +4,14 @@ import collections
 
 
 def main():
-    datafile = os.path.abspath('./data/datafile.csv')
+    datafile = os.path.abspath('./data/home_data.csv')
+    beds = 0
     HouseData = collections.namedtuple('HouseData', get_csv_header(datafile))
     banner()
+    get_csv_header(datafile)
+    find_least_expensive_house(datafile)
+    find_most_expensive_house(datafile)
+    calculate_average_house(beds)
 
 
 def banner():
@@ -17,7 +22,7 @@ def banner():
 def load_data():
     print('Loading CSV real estate data from ... {}'.format(datafile), end=' ')
     #TODO this is a CSV so making it over simple I am sure but hey
-    open(datafile, 'r') as fin:
+    with open(datafile, 'r') as fin:
        for id,line in enumerate(fin):
            if id == 0:
                continue
@@ -27,31 +32,38 @@ def load_data():
 
 def get_csv_header(datafile):
     print('Loading CSV real estate data from ... {} ... done'.format(datafile))
+    print()
     #TODO this is a CSV so making it over simple I am sure but hey
-    open(datafile, 'r') as fin:
+    with open(datafile, 'r') as fin:
        for id,line in enumerate(fin):
            if id == 0:
                print('Header: ' + line)
+               print()
                file_header = line
 
-        return file_header
+    return file_header
 
 
 def find_most_expensive_house(datafile):
+    line = 'todo'
     #TODO use HouseData to figure out most expensive house
     print('Most expensive house is: ' + line)
 
 def find_least_expensive_house(datafile):
     #TODO use HouseData to figure out least expansive house
+    line = 'todo'
     print('Least expensive house is: ' + line)
 
 def calculate_average_house(beds):
-    if beds = 0:
-    #TODO use HouseData to figure out averge house price
-    avg_price = 1
-    bed_count = 1
-    bath_count = 0
-    print('Average house: {}, {} bed, {} bath '.format(avg_price, bed_count, bath_count))
+    if beds == 0:
+        #TODO use HouseData to figure out averge house price
+        avg_price = 1
+        bed_count = 1
+        bath_count = 0
+        print('Average house: {}, {} bed, {} bath '.format(avg_price, bed_count, bath_count))
+    else:
+        print('TODO')
+
 
 
 
