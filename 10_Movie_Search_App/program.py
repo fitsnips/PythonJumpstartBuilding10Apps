e2b610abca827b258dacab5a0f719cbf800e82ee
Joshua Miller <jmiller@vera.com>
2018-07-18 13:25:40 -0500

    10 Movie Search App - init

---
+++ 10_Movie_Search_App/program.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+
+
+def main():
+    banner()
+
+
+def banner():
+    print('------------------------------------------------------')
+    print('                 Movie Search App                     ')
+    print('------------------------------------------------------')
+    print()
+
+
+if __name__ == '__main__':
+    main()
