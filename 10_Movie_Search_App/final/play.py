3fb2cf404838af7c75cefb699a6e020c2174cfdb
Joshua Miller <jmiller@vera.com>
2018-07-23 11:54:02 -0500

    10 Movie Search App - playground script, basic collection usage, phase 1

---
+++ 10_Movie_Search_App/final/play.py
@@ -0,0 +1,39 @@
+import requests
+import collections
+
+MoveResult = collections.namedtuple(
+    'MovieResult',
+    'imdb_code, title, duration,director, year, rating, imdb_score, keywords, generes'
+)
+
+search = 'capital'
+url = 'http://movie_service.talkpython.fm/api/search/{}'.format(search)
+
+
+resp = requests.get(url)
+resp.raise_for_status()
+
+movie_data = resp.json()
+movies_list = movie_data.get('hits')
+
+#print(type(movie_data), movie_data)
+#print(type(movies_list), movies_list)
+
+
+movies = []
+for md in movies_list:
+    m = MoveResult(
+        imdb_code=md.get('imdb_code'),
+        title=md.get('title'),
+        duration=md.get('duration'),
+        director=md.get('director'),
+        year=md.get('year', 0),
+        rating=md.get('rating', 0),
+        imdb_score=md.get('imdb_score', 0.0),
+        keywords=md.get('keywords'),
+        generes=md.get('generes')
+    )
+    movies.append(m)
+
+for m in movies:
+    print("{} -- {}".format(m.year, m.title))
--- 10_Movie_Search_App/program.py
+++ 10_Movie_Search_App/program.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-
-
+import urllib3
 
 
 def main():
     banner()
+    search_title = get_movie_title()
+    found_titles = find_movie_by_title(search_title)
+    print_found_titles(found_titles)
 
 
 def banner():
@@ -13,6 +15,20 @@ def banner():
     print('------------------------------------------------------')
     print()
 
+def get_movie_title():
+    title = input=('What movie title would you like to search for: ')
+    return title
+
+def find_movie_by_title(search_string):
+    url = movie_service.talkpython.fm/search_string
+
+    try
+    titles = { 'name': title, 'year': year }
+    return titles
+
+def print_found_titles(titles):
+    for year, title in titles:
+        print('{} - {}'.format(year, title))
 
 if __name__ == '__main__':
     main()
